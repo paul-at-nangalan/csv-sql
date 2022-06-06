f3a363a88cc717955506e9f56b1340f31de6ac10
paul reed <paul@nesystemsasia.com>
2022-06-06 18:14:55 +0800

    added int cast

--- transform/cast/transform.go
+++ transform/cast/transform.go
@@ -10,6 +10,7 @@ import (
 
 const(
 	TOFLOAT = "float"
+	TOINT = "int"
 )
 
 type Cast struct{
@@ -65,12 +66,50 @@ func toFloat(v interface{})(float64, error){
 	return 0, errors.New(fmt.Sprint("Failed to convert", v, " to float"))
 }
 
+func toInt(v interface{})(int64, error){
+	switch i := v.(type) {
+	case int64:
+		return i, nil
+	case int32:
+		return int64(i), nil
+	case float64:
+		return int64(i), nil
+	case float32:
+		return int64(i), nil
+	case int:
+		return int64(i), nil
+	case uint64:
+		return int64(i), nil
+	case uint32:
+		return int64(i), nil
+	case uint:
+		return int64(i), nil
+	case string:
+		parsed := strings.TrimSpace(i)
+		if parsed == ""{
+			return 0, nil
+		}
+		parsed = strings.Replace(parsed, ",", "", -1)
+		return strconv.ParseInt(parsed, 10, 64)
+	case nil:
+		return 0, nil
+	}
+	return 0, errors.New(fmt.Sprint("Failed to convert", v, " to int"))
+}
+
 func (p *Cast) Do(vals []interface{}) ([]interface{}, error) {
 	for i, val := range vals{
-		if p.casts[i] == TOFLOAT{
+		switch p.casts[i] {
+		case TOFLOAT:
 			//fmt.Println("Format ", vals[i], " to ", p.casts[i])
 			fval, err := toFloat(val)
-			if err != nil{
+			if err != nil {
+				return nil, err
+			}
+			vals[i] = fval
+		case TOINT:
+			fval, err := toInt(val)
+			if err != nil {
 				return nil, err
 			}
 			vals[i] = fval
